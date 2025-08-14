b45ce23baafcdf7b598c7bca77d6c226a42ff80f
Leonardo <dalolmo.leo@gmail.com>
2025-08-14 15:53:07 -0300

    Initial commit

---
+++ src/test/java/com/example/Controle_Gastos/ControleGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Controle_Gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
