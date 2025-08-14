323881bece176a0a16b5f7235be01d3ce1f401ce
Leonardo <dalolmo.leo@gmail.com>
2025-08-14 19:18:12 -0300

    feat:Criado classe DTO de Transação.

--- src/main/java/com/example/Controle_Gastos/dto/TranscaoDTO.java
+++ src/main/java/com/example/Controle_Gastos/dto/TranscaoDTO.java
@@ -1,87 +1,68 @@
-package com.example.Controle_Gastos.entities;
+package com.example.Controle_Gastos.dto;
 
+import com.example.Controle_Gastos.entities.Transacao;
 import com.example.Controle_Gastos.enums.TipoTransacao;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "tb_transacao")
-public class Transacao {
+public class TranscaoDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O nome da transação não pode ser nulo")
     private String nome;
 
     private String descricao;
 
+    @NotNull
+    @Positive
     private Double valor;
 
     private TipoTransacao tipoTransacao;
 
     private LocalDate data;
 
-    public Transacao() {
-    }
-
-    public Transacao(Long id, String nome, String descricao, Double valor, TipoTransacao tipoTransacao, LocalDate data) {
+    public TranscaoDTO(Long id, String nome, String descricao, Double valor, TipoTransacao tipoTransacao, LocalDate data) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.valor = valor;
         this.tipoTransacao = tipoTransacao;
         this.data = data;
+    }
+
+    public TranscaoDTO(Transacao entity) {
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.descricao = entity.getDescricao();
+        this.valor = entity.getValor();
+        this.tipoTransacao = entity.getTipoTransacao();
+        this.data = entity.getData();
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public Double getValor() {
         return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
     }
 
     public TipoTransacao getTipoTransacao() {
         return tipoTransacao;
     }
 
-    public void setTipoTransacao(TipoTransacao tipoTransacao) {
-        this.tipoTransacao = tipoTransacao;
-    }
-
     public LocalDate getData() {
         return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
     }
 }
